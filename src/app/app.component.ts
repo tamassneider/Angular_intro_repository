ea8b91e011f790f45c195a44e626137b69b1369c
tamassneider <tamassneider90@gmail.com>
2018-03-26 09:39:08 +0200

    first of angular intro project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'netacademia123';
+  tomb = [1,2,3,4,5,6,7,];
+  constructor(){
+    console.log(this.tomb.filter((param: number) => param === 2));
+    console.log(this.tomb.map((param:number) => param * 2));
+    console.log(this.tomb.reduce((x,y) => x+y));
+  }
 }
