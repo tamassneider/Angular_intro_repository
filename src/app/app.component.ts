dda5a5eaedf16d4c1c82724f89968d6be6ef000f
tamassneider <tamassneider90@gmail.com>
2018-03-26 17:54:29 +0200

    -added ng functions and examples of data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'netacademia123';
-  tomb = [1,2,3,4,5,6,7,];
-  constructor(){
+  tomb = [1, 2, 3, 4, 5, 6, 7];
+  events = ['sziget', 'effott', 'volt' ];
+  show = true;
+  whenclicked = false;
+  inputContent: string;
+
+  toggle() {
+    this.show = !this.show;
+    this.inputContent = 'bla bla bla';
+  }
+
+  demo(ev: MouseEvent) {
+    console.log(ev.screenX);
+  }
+
+  constructor() {
     console.log(this.tomb.filter((param: number) => param === 2));
-    console.log(this.tomb.map((param:number) => param * 2));
-    console.log(this.tomb.reduce((x,y) => x+y));
+    console.log(this.tomb.map((param: number) => param * 2));
+    console.log(this.tomb.reduce((x, y) => x + y));
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EventComponent } from './event/event.component';
 import {Event2Component} from './event_created_manually/event2.component';
-
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import {Event2Component} from './event_created_manually/event2.component';
     Event2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
